170d119aaa17e897f8619953040bb237f061bfd9
Feliciti Huarancca <38644048+feliciti-huarancca@users.noreply.github.com>
2025-09-10 09:41:26 -0600

    Initial commit

---
+++ challenge_2.js
@@ -0,0 +1,9 @@
+// 2. Create a function that return the lenght of a string
+
+function getStringLength(str) {   
+  // Add code here to return the length of str
+  return str.length;
+}
+
+console.log(getStringLength('sergii')) // 6
+console.log(getStringLength('alex')) // 4
---
+++ taskExample.js
@@ -0,0 +1,22 @@
+// Task example! Concatenate 2 variables to create a sentence of your preference:
+
+// Solution
+const name = 'Sergii' 
+const greeting = 'Welcome to the team, '
+
+console.log(greeting + name)
+
+//Task example! Output the length of the string “Sergii“
+
+// Solution:
+console.log("sergii".length)
+
+// FYI! Same thing can be done this way:
+const name2 = "sergii"
+console.log(name2.length)
+
+//Task example! Create an “if statement” that checks if 5 is bigger than 4:
+// Solution
+if(5 > 4) {
+  console.log('5 is bigger')
+}
